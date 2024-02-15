69d6942d5b1da8a87bab2983e91bca09e64f195f
ErshadGS <ershadzh7@gmail.com>
2024-02-14 21:31:47 -0500

    Added textures and models

--- HandgeonCrawler/main.cpp
+++ HandgeonCrawler/main.cpp
@@ -3,7 +3,7 @@
 #include "Random.h"
 #include <iostream>
 #include <map>
-#include "ConfigReader.h"
+#include "LevelReader.h"
 
 extern Camera* camera;
 int keyHit = 0;
@@ -13,8 +13,11 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
     if (action == GLFW_PRESS)
     {
         keyHit = key;
-        return;
     }
+    else if (action == GLFW_RELEASE)
+    {
+		keyHit = 0;
+	}
 }
 
 void mouse_callback(GLFWwindow* window, double xpos, double ypos)
@@ -30,7 +33,7 @@ int main(void)
         return 1;
     }
 
-    engine.meshManager->LoadTexture("PaletteV1.bmp");
+    engine.meshManager->LoadTexture("Water_Texture_01.bmp");
     bool loaded = engine.meshManager->LoadCubeMap("space",
         "CubeMaps/TropicalSunnyDayLeft2048.bmp",
         "CubeMaps/TropicalSunnyDayRight2048.bmp",
@@ -44,14 +47,71 @@ int main(void)
     skyBoxMesh->isSkyBox = true;
     skyBoxMesh->setUniformDrawScale(5000.0f);
 
+    //Flat_1x1_plane
+    cMesh* plane = engine.LoadMesh("Flat_1x1_plane.ply", "plane");
+    plane->setUniformDrawScale(1000.0f);
+    plane->texture[0] = "Water_Texture_01.bmp";
+    plane->textureRatio[0] = 1.0f;
     camera->SetPosition(glm::vec3(0.0f, 400.0f, 200.0f));
+
+    //read the level from json
+    LevelReader levelReader(engine);
+    LevelData levelData;
+    levelReader.LoadLevel(levelData);
 
     float currTime = 0;
     float myTime = 0;
 
+    float playerSpeed = 0;
+
+    for (size_t i = 0; i < levelData.characters.size(); i++)
+    {
+        Character* character = levelData.characters[i];
+        if (character->name == "Player")
+        {
+            playerSpeed = character->speed;
+        }
+    }
+
+    cMesh* player = engine.meshManager->FindMeshByFriendlyName("Player");
+
+    player->drawScale = glm::vec3(20, 20, -20);
+
     while (!glfwWindowShouldClose(engine.window))
     {
         engine.Update();
+        //rotate camera
+         glm::vec3 cameraRot = glm::vec3(camera->pitch / 100.0f, -camera->yaw / 100.0f, 0);
+
+        //follow the player
+        camera->Follow(player->drawPosition, glm::vec3(0, 0.3f, 1),
+            player->drawPosition, cameraRot);
+
+        player->setRotationFromEuler(glm::vec3(0, -camera->yaw / 100.0f, 0));
+
+        //get the forward and right vectors of the player
+        glm::vec3 forward = player->GetForwardVector();
+        forward = glm::vec3(forward.x, 0, forward.z); // Flatten the forward vector
+        glm::vec3 right = player->GetRightVector();
+
+
+        //move the player
+        if (keyHit == GLFW_KEY_W)
+        {
+            player->drawPosition += playerSpeed * -forward;
+        }
+        else if (keyHit == GLFW_KEY_S)
+        {
+            player->drawPosition += playerSpeed * forward;
+        }
+        else if (keyHit == GLFW_KEY_A)
+        {
+            player->drawPosition += playerSpeed * right;
+        }
+        else if (keyHit == GLFW_KEY_D)
+        {
+            player->drawPosition += playerSpeed * -right;
+        }
     }
 
     engine.ShutDown();
