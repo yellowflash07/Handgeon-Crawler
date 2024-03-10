bd0cb3ebe4e1fba1b23054ab2bfec331c070fc27
ErshadGS <ershadzh7@gmail.com>
2024-03-09 21:47:53 -0500

    update input

--- HandgeonCrawler/Input.h
+++ HandgeonCrawler/Input.h
@@ -1,5 +1,22 @@
 #pragma once
+#include <map>
 struct Input
 {
+
+    std::map<int, bool> keys;
+
+    bool isKeyPressed(int key)
+    {
+        if (keys[key])
+        {
+			keys[key] = false;
+			return true;
+		}
+		return false;
+	}
+
     int keyHit;
+    int scancode;
+    int action;
+    int mods;
 };
